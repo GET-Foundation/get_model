98414fc504e17b1597ce5a6426b72d494030f6f7
fuxialexander <fuxialexander@gmail.com>
2023-12-25 00:28:19 -0500

    cleanup

--- get_model/dataset/zarr_dataset.py
+++ get_model/dataset/zarr_dataset.py
@@ -20,53 +20,9 @@ from scipy.sparse import coo_matrix, load_npz, vstack
 import zarr
 from torch.utils.data import Dataset
 from tqdm import tqdm
-from caesar.io.zarr_io import SequenceDenseZarrIO
+from caesar.io.zarr_io import DenseZarrIO, CelltypeDenseZarrIO
 from numcodecs import Blosc
 #%%
-
-class CelltypeDenseZarrIO(object):
-    """
-    Class for handling dense data in Zarr format.
-    Directly use zarr index to get the data.
-    """
-
-    def __init__(self, path: str):
-        self.data_key = basename(path)
-        self.dataset = zarr.open_group(path, mode='r')
-        self.ids = list(self.dataset.keys())
-        self.celltypes = [self.dataset[id].attrs['celltype'] for id in self.ids]
-        self.n_samples = [self.dataset[id].attrs['n_sample'] for id in self.ids]
-        self.n_celltypes = len(self.ids)
-        self.chunk_size = self.dataset[self.ids[0]]['chr1']['chunk_1'].shape[0]
-        self.chrom_n_chunks = {}
-        for chr_name in self.dataset[self.ids[0]].keys():
-            self.chrom_n_chunks[chr_name] = len(self.dataset[self.ids[0]][chr_name])
-
-    def get_track(self, celltype, chr_name: str, start: int, end: int) -> np.ndarray:
-        """
-        Get the track from the zarr file
-
-        Args:
-            celltype (str): celltype name
-            chr_name (str): chromosome name
-            start (int): start position
-            end (int): end position
-
-        Returns:
-            np.ndarray: track
-        """
-        chunk_start = start // self.chunk_size
-        chunk_end = end // self.chunk_size + 1
-        track = []
-        for chunk_idx in range(chunk_start, chunk_end):
-            chunk = self.dataset[celltype][chr_name][f'chunk_{chunk_idx}']
-            c_start = chunk_idx * self.chunk_size
-            track.append(chunk[start - c_start:end - c_start])
-        return np.concatenate(track)
-    
-    def sample_track(self, chr_name: str, start: int, end: int) -> np.ndarray:
-        cell_type = np.random.choice(self.celltypes)
-        return self.get_track(cell_type, chr_name, start, end)
 
 
 
@@ -80,7 +36,7 @@ class PretrainDataset(Dataset):
     def __init__(self, zarr_dirs, genome_seq_zarr, insulation_path) -> None:
         from glob import glob
         super().__init__()
-        self.sequence = SequenceDenseZarrIO(genome_seq_zarr+'/chrs')
+        self.sequence = DenseZarrIO(genome_seq_zarr+'/chrs')
         self.sequence.load_to_memory()
         self.zarr_dirs = zarr_dirs
         self.zarr_dict = {}
