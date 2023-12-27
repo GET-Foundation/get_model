df251054aac96bfa8234731468b5dfa5d7dbf33d
fuxialexander <fuxialexander@gmail.com>
2023-12-27 12:57:39 -0500

    update

--- get_model/dataset/collate.py
+++ get_model/dataset/collate.py
@@ -32,3 +32,22 @@ def sparse_batch_collate(batch: list):
     return data_batch, targets_batch, cells_batch, ctcf_pos_batch
 
 
+def csr_to_torch_sparse(csr):
+    # Step 1: Extract CSR components
+    csr_data = csr.data
+    csr_indices = csr.indices
+    csr_indptr = csr.indptr
+
+    # Step 2: Convert CSR to COO
+    coo_row_indices = np.empty_like(csr_data, dtype=np.int64)
+    for i in range(len(csr_indptr) - 1):
+        start, end = csr_indptr[i], csr_indptr[i+1]
+        coo_row_indices[start:end] = i  # Fill row indices
+    coo_col_indices = csr_indices
+    coo_indices = np.vstack((coo_row_indices, coo_col_indices))
+
+    # Step 3: Create a PyTorch sparse tensor
+    coo_indices = torch.LongTensor(coo_indices)
+    coo_data = torch.FloatTensor(csr_data)
+    size = torch.Size(csr.shape)
+    return coo_indices, coo_data, size
--- test/test_zarr_dataset.py
+++ test/test_zarr_dataset.py
@@ -4,7 +4,7 @@ from get_model.dataset.zarr_dataset import PretrainDataset
 import torch
 import numpy as np
 from scipy.sparse import coo_matrix
-from get_model.dataset.collate import sparse_coo_to_tensor
+from get_model.dataset.collate import csr_to_torch_sparse
 # %%
 pretrain = PretrainDataset(['/pmglocal/xf2217/shendure_fetal/shendure_fetal_dense.zarr', 
                             '/pmglocal/xf2217/bingren_adult/bingren_adult_dense.zarr'], 
@@ -19,6 +19,50 @@ pretrain.__len__()
 # data = pretrain.__getitem__(1)
 
 # %%
+
+
+from scipy.sparse import csr_matrix
+import numpy as np
+
+def remove_consecutive_zero_rows(csr, threshold=100):
+    non_zero_per_row = csr.getnnz(axis=1)
+    is_zero_row = non_zero_per_row == 0
+
+    # Add padding for edge cases
+    padded_is_zero_row = np.concatenate([[False], is_zero_row, [False]])
+
+    diff = np.diff(padded_is_zero_row.astype(int))
+    start_indices = np.where(diff == 1)[0]
+    end_indices = np.where(diff == -1)[0]
+
+    long_sequences = (end_indices - start_indices) > threshold
+
+    # Compute the boundary indices in the new matrix
+    boundary_indices = []
+    prev_end = 0
+    for start, end in zip(start_indices, end_indices):
+        if start - prev_end > 0 and not (start in start_indices[long_sequences] and end in end_indices[long_sequences]):
+            boundary_indices.append((prev_end, start))
+        prev_end = end
+
+    # Get indices to keep
+    all_indices = np.arange(csr.shape[0])
+    for start, end in zip(start_indices[long_sequences], end_indices[long_sequences]):
+        all_indices = np.setdiff1d(all_indices, np.arange(start, end))
+
+    # Convert boundary indices to new indices
+    new_boundaries = []
+    for start, end in boundary_indices:
+        new_start = np.searchsorted(all_indices, start)
+        new_boundaries.append((new_start))
+
+    # Return the filtered csr_matrix and new boundary indices
+    return csr[all_indices], np.array(new_boundaries)
+
+# Example usage
+# csr = csr_matrix(...)
+# csr_filtered, boundary_indices = remove_consecutive_zero_rows(csr, threshold=30)
+
 def collate_fn_test(batch):
     # zip and convert to list
     sample_track, sample_peak_sequence, sample_metadata, celltype_peaks = zip(*batch)
@@ -28,11 +72,17 @@ def collate_fn_test(batch):
     sample_metadata = list(sample_metadata)
     batch_size = len(celltype_peaks)
     n_peak_max = max([len(x) for x in celltype_peaks])
-    sample_len_max = max([x.shape[0] for x in sample_track])
+    sample_len_max = max([len(x.getnnz(1)) for x in sample_peak_sequence])
+    sample_track_boundary = []
+    sample_peak_sequence_boundary = []
     # pad each peaks in the end with 0
     for i in range(len(celltype_peaks)):
         celltype_peaks[i] = np.pad(celltype_peaks[i], ((0, n_peak_max - len(celltype_peaks[i])), (0,0)))
         # pad each track in the end with 0 which is csr_matrix, use sparse operation
+        sample_track[i], boundary = remove_consecutive_zero_rows(sample_track[i])
+        sample_track_boundary.append(boundary)
+        sample_peak_sequence[i], boundary = remove_consecutive_zero_rows(sample_peak_sequence[i])
+        sample_peak_sequence_boundary.append(boundary)
         sample_track[i].resize((sample_len_max, sample_track[i].shape[1]))
         sample_peak_sequence[i].resize((sample_len_max, sample_peak_sequence[i].shape[1]))
         sample_track[i] = sample_track[i].todense()
@@ -43,14 +93,15 @@ def collate_fn_test(batch):
     sample_track = np.hstack(sample_track).T
     sample_track = torch.from_numpy(sample_track)
     sample_peak_sequence = np.hstack(sample_peak_sequence)
-    sample_peak_sequence = torch.from_numpy(sample_peak_sequence)
-    return sample_track, sample_peak_sequence.view(-1, batch_size, 4), sample_metadata, celltype_peaks
-
+    sample_peak_sequence = torch.from_numpy(sample_peak_sequence).view(-1, batch_size, 4)
+    # sample_peak_sequence = sample_peak_sequence.to_sparse_csr()
+    # return sample_track, sample_peak_sequence.view(-1, batch_size, 4), sample_metadata, celltype_peaks
+    return sample_track, sample_peak_sequence, sample_metadata, celltype_peaks, sample_track_boundary, sample_peak_sequence_boundary
 #%%
 data_loader_train = torch.utils.data.DataLoader(
     pretrain,
     batch_size=64,
-    num_workers=8,
+    num_workers=32,
     pin_memory=False,
     drop_last=True,
     collate_fn = collate_fn_test
@@ -58,7 +109,7 @@ data_loader_train = torch.utils.data.DataLoader(
 
 # %%
 for batch in tqdm(data_loader_train):
-    sample_track, sample_peak_sequence, sample_metadata, celltype_peaks = batch
+    sample_track, sample_peak_sequence, sample_metadata, celltype_peaks, sample_track_boundary, sample_peak_sequence_boundary= batch
 # %%
 celltype_peaks.shape
 # %%
