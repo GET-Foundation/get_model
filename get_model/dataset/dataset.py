0462cf49415af19f8c8bca4004b0950f507070ae
Alejandro Buendia <alb2281@columbia.edu>
2024-01-25 10:15:24 -0500

    GBM/RCC finetune script

--- get_model/dataset/dataset.py
+++ get_model/dataset/dataset.py
@@ -307,8 +307,9 @@ def build_dataset_zarr(is_train, args, sequence_obj=None):
         print("Data Aug = %s" % str(transform))
         root = args.data_path
         # get FILEPATH
-        codebase = os.path.dirname(os.path.dirname(
-            os.path.dirname(os.path.abspath(__file__))))
+        # codebase = os.path.dirname(os.path.dirname(
+        #    os.path.dirname(os.path.abspath(__file__))))
+        codebase = "/pmglocal/alb2281/repos/get_model"
         if sequence_obj is None:
             sequence_obj = DenseZarrIO(f'{root}/hg38.zarr', dtype='int8')
             sequence_obj.load_to_memory_dense()
@@ -327,8 +328,9 @@ def build_dataset_zarr(is_train, args, sequence_obj=None):
         print("Data Aug = %s" % str(transform))
         root = args.data_path
         # get FILEPATH
-        codebase = os.path.dirname(os.path.dirname(
-            os.path.dirname(os.path.abspath(__file__))))
+        # codebase = os.path.dirname(os.path.dirname(
+        #    os.path.dirname(os.path.abspath(__file__))))
+        codebase = "/pmglocal/alb2281/repos/get_model"
         if sequence_obj is None:
             sequence_obj = DenseZarrIO(f'{root}/hg38.zarr', dtype='int8')
             sequence_obj.load_to_memory_dense()
@@ -369,6 +371,51 @@ def build_dataset_zarr(is_train, args, sequence_obj=None):
                            peak_name=args.peak_name, insulation_subsample_ratio=0.8, additional_peak_columns=['Expression_positive', 'Expression_negative'], preload_count=args.preload_count, 
                            n_packs=args.n_packs, max_peak_length=args.max_peak_length, center_expand_target=args.center_expand_target, n_peaks_lower_bound=args.n_peaks_lower_bound, n_peaks_upper_bound=args.n_peaks_upper_bound, use_insulation=args.use_insulation, sequence_obj=sequence_obj, leave_out_celltypes=args.leave_out_celltypes,
                            leave_out_chromosomes=None, is_train=is_train, non_redundant=None, dataset_size=4096)
+    elif is_train and args.data_set == "HTAN_GBM":
+        transform = DataAugmentationForGETPeak(args)
+        print("Data Aug = %s" % str(transform))
+        root = args.data_path
+        # get FILEPATH
+        codebase = os.path.dirname(os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))))
+        if sequence_obj is None:
+            sequence_obj = DenseZarrIO(f'{root}/hg38.zarr', dtype='int8')
+            sequence_obj.load_to_memory_dense()
+        else:
+            logging.info('sequence_obj is provided')
+        logging.info('Using HTAN_GBM') 
+        dataset = ZarrPretrainDataset([
+            f'{root}/htan_gbm_dense.zarr',
+            ],
+                           f'/pmglocal/alb2281/fetal_adult_data/hg38.zarr',
+                           f'/pmglocal/alb2281/fetal_adult_data/hg38_motif_result.zarr', 
+                           ['/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.adjecent.feather', '/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.longrange.feather'], 
+                           peak_name=args.peak_name, insulation_subsample_ratio=0.8,
+                           additional_peak_columns=['Expression_positive', 'Expression_negative'], preload_count=args.preload_count, 
+                           n_packs=args.n_packs, max_peak_length=args.max_peak_length, center_expand_target=args.center_expand_target, n_peaks_lower_bound=args.n_peaks_lower_bound, n_peaks_upper_bound=args.n_peaks_upper_bound, use_insulation=args.use_insulation, sequence_obj=sequence_obj, leave_out_celltypes=args.leave_out_celltypes, leave_out_chromosomes=args.leave_out_chromosomes, is_train=is_train, non_redundant="max_depth", dataset_size=65536)
+    elif not is_train and args.eval_data_set == "HTAN_GBM.eval":
+        transform = DataAugmentationForGETPeak(args)
+        print("Data Aug = %s" % str(transform))
+        root = args.data_path
+        # get FILEPATH
+        codebase = os.path.dirname(os.path.dirname(
+            os.path.dirname(os.path.abspath(__file__))))
+        if sequence_obj is None:
+            sequence_obj = DenseZarrIO(f'{root}/hg38.zarr', dtype='int8')
+            sequence_obj.load_to_memory_dense()
+        else:
+            logging.info('sequence_obj is provided')
+
+        logging.info('Using HTAN_GBM.eval')
+        dataset = ZarrPretrainDataset([
+            f"{root}/htan_gbm_dense.zarr",
+            ],
+                           f'/pmglocal/alb2281/fetal_adult_data/hg38.zarr',
+                           f'/pmglocal/alb2281/fetal_adult_data/hg38_motif_result.zarr', 
+                           ['/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.adjecent.feather', '/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.longrange.feather'], 
+                           peak_name=args.peak_name, insulation_subsample_ratio=0.8, additional_peak_columns=['Expression_positive', 'Expression_negative'], preload_count=args.preload_count, 
+                           n_packs=args.n_packs, max_peak_length=args.max_peak_length, center_expand_target=args.center_expand_target, n_peaks_lower_bound=args.n_peaks_lower_bound, n_peaks_upper_bound=args.n_peaks_upper_bound, use_insulation=args.use_insulation, sequence_obj=sequence_obj, leave_out_celltypes=args.leave_out_celltypes,
+                           leave_out_chromosomes=args.leave_out_chromosomes, is_train=is_train, non_redundant="max_depth", dataset_size=4096)
         
     else:
         raise NotImplementedError()
--- test/test_finetune_data.py
+++ test/test_finetune_data.py
@@ -1,45 +1,35 @@
-# %%
-import logging
-from matplotlib import pyplot as plt
-
 import numpy as np
 import seaborn as sns
 import torch
 from scipy.sparse import coo_matrix
 from tqdm import tqdm
+import wandb
+import torch.nn as nn
+import matplotlib.pyplot as plt
 
 from get_model.dataset.collate import get_rev_collate_fn
 from get_model.dataset.zarr_dataset import PretrainDataset, ZarrDataPool, PreloadDataPack, CelltypeDenseZarrIO
-
-# %%
-cdz = CelltypeDenseZarrIO('/pmglocal/xf2217/get_data/shendure_fetal_dense.zarr')
-# %%
-cdz = cdz.subset_celltypes_with_data_name()
-#%%
-cdz = cdz.leave_out_celltypes_with_pattern('Astrocyte')
-
-
-
-#%%
-pretrain = PretrainDataset(['/pmglocal/xf2217/get_data/shendure_fetal_dense.zarr',
-                            ],
-                           '/pmglocal/xf2217/get_data/hg38.zarr', 
-                           '/pmglocal/xf2217/get_data/hg38_motif_result.zarr', [
-                           '/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.adjecent.feather', '/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.longrange.feather'], peak_name='peaks_q0.01_tissue_open_exp', preload_count=200, n_packs=1,
-                           max_peak_length=5000, center_expand_target=1000, n_peaks_lower_bound=50, n_peaks_upper_bound=100, leave_out_celltypes='Enterocyte', leave_out_chromosomes='chr11', is_train=False, additional_peak_columns=['Expression_positive', 'Expression_negative'], non_redundant='max_depth', use_insulation=False)
-pretrain.__len__()
-# %%
-pretrain.datapool.insulation
-# %%
-list(pretrain.datapool.peaks_dict.keys())
-# %%
-pretrain.preload_data_packs = ['']
-pretrain.reload_data(0)
-
-# %%
-pretrain.__getitem__(10)
-# %%
 from get_model.dataset.zarr_dataset import worker_init_fn_get
+from get_model.model.model import GETFinetune  
+from get_model.utils import load_state_dict
+from get_model.dataset.zarr_dataset import get_mask_pos, get_padding_pos
+
+
+wandb.login()
+run = wandb.init(
+    project="get",
+    name="finetune-gbm",
+)
+
+pretrain = PretrainDataset(['/pmglocal/alb2281/geo_data/htan_final/gbm/htan_gbm_dense.zarr',
+                            ],
+                           '/pmglocal/alb2281/fetal_adult_data/hg38.zarr', 
+                           '/pmglocal/alb2281/fetal_adult_data/hg38_motif_result.zarr', [
+                           '/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.adjecent.feather', '/manitou/pmg/users/xf2217/get_model/data/hg38_4DN_average_insulation.ctcf.longrange.feather'], peak_name='peaks_q0.01_tissue_open_exp', preload_count=200, n_packs=1,
+                           max_peak_length=5000, center_expand_target=1000, n_peaks_lower_bound=50, n_peaks_upper_bound=100, leave_out_celltypes=None, leave_out_chromosomes='chr11', is_train=False, additional_peak_columns=['Expression_positive', 'Expression_negative'], non_redundant='max_depth', use_insulation=False)
+pretrain.__len__()
+
+
 data_loader_train = torch.utils.data.DataLoader(
     pretrain,
     batch_size=8,
@@ -50,11 +40,8 @@ data_loader_train = torch.utils.data.DataLoader(
     worker_init_fn=worker_init_fn_get,
 )
 
-# %%
-from get_model.model.model import GETFinetune  
-from get_model.utils import load_state_dict
-import torch.nn as nn
 loss_masked = nn.PoissonNLLLoss(log_input=False, reduce='mean')
+criterion = nn.PoissonNLLLoss(log_input=False, reduce='mean')
 model = GETFinetune(
         num_regions=100,
         num_res_block=0,
@@ -62,40 +49,32 @@ model = GETFinetune(
         embed_dim=768,
         num_layers=12,
         d_model=768,
-        flash_attn=True,
+        flash_attn=False,
         nhead=12,
         dropout=0.1,
         output_dim=2,
         pos_emb_components=[],
     )
-#%%
-checkpoint = torch.load('/pmglocal/xf2217/output_rev_from_scratch_ATACSplitPool_unnorm_finetune_fetal_Erythroblast_leaveout_chr_bidirectional/checkpoint-135.pth')
-#%%
-model.load_state_dict(checkpoint["model"], strict=True)
 
-#%%
+checkpoint = torch.load('/pmglocal/alb2281/get_ckpts/checkpoint-135.pth')
+model.load_state_dict(checkpoint["model"], strict=True)
 model.eval()
 model.cuda()
-#%%
+
 for i, j in model.atac_attention.joint_conv.named_parameters():
     print(i, j.requires_grad)
     weight = j.detach().cpu().numpy()
-#%%
-import matplotlib.pyplot as plt
+
 plt.imshow(weight[:,0,:])
-#%%
-# plot as six line plot
 for i in range(6):
     plt.imshow(weight[i,0:100,:])
-#%%
-from get_model.dataset.zarr_dataset import get_mask_pos, get_padding_pos
 
 def train_class_batch(model, peak_seq, sample_track, mask, chunk_size, n_peaks, max_n_peaks, motif_mean_std, atac_target, exp_target, criterion):
     device = peak_seq.device
     padding_mask = get_padding_pos(mask)
     mask_for_loss = 1-padding_mask
     padding_mask = padding_mask.to(device, non_blocking=True).bool()
-    print(padding_mask.sum())
+    # print(padding_mask.sum())
     mask_for_loss = mask_for_loss.to(device, non_blocking=True).unsqueeze(-1)
     with torch.cuda.amp.autocast(enabled=True, dtype=torch.bfloat16):
         atac, exp = model(peak_seq, sample_track, mask_for_loss, padding_mask, chunk_size, n_peaks, max_n_peaks, motif_mean_std)
@@ -105,19 +84,14 @@ def train_class_batch(model, peak_seq, sample_track, mask, chunk_size, n_peaks, 
     exp_target = exp_target * mask_for_loss
     exp_target = exp_target[indices[0], indices[1], :].flatten()
     loss_exp = criterion(exp, exp_target)
-    # loss = loss_atac + loss_exp
     loss = loss_exp
     return loss, atac, exp, exp_target 
-    # loss_atac = criterion(atac, atac_target)
-criterion = nn.PoissonNLLLoss(log_input=False, reduce='mean')
-#%%
+
 losses = []
 preds = []
 obs = []
 with torch.amp.autocast('cuda', dtype=torch.bfloat16):
     for i, batch in tqdm(enumerate(data_loader_train)):
-        if i > 100:
-            break
         sample_track, peak_seq, sample_metadata, celltype_peaks, sample_track_boundary, sample_peak_sequence_boundary, chunk_size, mask, n_peaks, max_n_peaks, total_peak_len, motif_mean_std, labels_data = batch
         if min(chunk_size)<0:
             continue
@@ -125,32 +99,21 @@ with torch.amp.autocast('cuda', dtype=torch.bfloat16):
         sample_track = sample_track.to(device, non_blocking=True).bfloat16()
         peak_seq = peak_seq.to(device, non_blocking=True).bfloat16()
         motif_mean_std = motif_mean_std.to(device, non_blocking=True).bfloat16()
-        # chunk_size = chunk_size.to(device, non_blocking=True)
         n_peaks = n_peaks.to(device, non_blocking=True)
         labels_data = labels_data.to(device, non_blocking=True).bfloat16()
 
         # compute output
-        loss, atac, exp, exp_target = train_class_batch(model, peak_seq, sample_track, mask, chunk_size, n_peaks, max_n_peaks, motif_mean_std
-        , None, labels_data, criterion)
+        loss, atac, exp, exp_target = train_class_batch(model, peak_seq, sample_track, mask, chunk_size, n_peaks, max_n_peaks, motif_mean_std, None, labels_data, criterion)
 
         preds.append(exp.reshape(-1).detach().cpu().numpy())
         obs.append(exp_target.reshape(-1).detach().cpu().numpy())
-        # preds_atac.append(atac.reshape(-1).detach().cpu().numpy())
-        # obs_atac.append(atac_targets.reshape(-1).detach().cpu().numpy())
-
-        # metric_logger.update(loss=loss.item())
 
     preds = np.concatenate(preds, axis=0).reshape(-1)
     obs = np.concatenate(obs, axis=0).reshape(-1)
-    # preds_atac = np.concatenate(preds_atac, axis=0).reshape(-1)
-    # obs_atac = np.concatenate(obs_atac, axis=0).reshape(-1)
-# %%
 
 sns.scatterplot(x=preds, y=obs, s=2, alpha=1)
 # add correlation as text
 corr = np.corrcoef(preds, obs)[0,1]
 corr = round(corr, 2)
 plt.title(f'Correlation: {corr}')
-# %%
-np.mean(losses)
-# %%
+print(losses)
