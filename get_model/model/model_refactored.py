890d0cd164fba44bf4ec7e27ccdcd87caad9337f
Xi Fu <fuxialexander@gmail.com>
2024-04-08 15:23:27 -0400

    chrombpnet update

--- get_model/model/model_refactored.py
+++ get_model/model/model_refactored.py
@@ -22,11 +22,79 @@ from get_model.model.transformer import GETTransformer
 
 class MNLLLoss(nn.Module):
     def __init__(self):
-        super(MNLLLoss, self).__init__()
+        """
+From @jmschrei/bpnet-lite
+A loss function based on the multinomial negative log-likelihood.
 
-    def forward(self, x, y):
-        """Compute the loss use -y* log(softmax(x))"""
-        return (-y.squeeze(1) * F.log_softmax(x, dim=-1)).mean()
+    This loss function takes in a tensor of normalized log probabilities such
+    that the sum of each row is equal to 1 (e.g. from a log softmax) and
+    an equal sized tensor of true counts and returns the probability of
+    observing the true counts given the predicted probabilities under a
+    multinomial distribution. Can accept tensors with 2 or more dimensions
+    and averages over all except for the last axis, which is the number
+    of categories.
+
+    Adapted from Alex Tseng.
+
+    Parameters
+    ----------
+    logps: torch.tensor, shape=(n, ..., L)
+            A tensor with `n` examples and `L` possible categories.
+
+    true_counts: torch.tensor, shape=(n, ..., L)
+            A tensor with `n` examples and `L` possible categories.
+
+    Returns
+    -------
+    loss: float
+            The multinomial log likelihood loss of the true counts given the
+            predicted probabilities, averaged over all examples and all other
+            dimensions.
+        """
+        super().__init__()
+
+    def forward(self, logps, true_counts):
+        log_fact_sum = torch.lgamma(torch.sum(true_counts, dim=-1) + 1)
+        log_prod_fact = torch.sum(torch.lgamma(true_counts + 1), dim=-1)
+        log_prod_exp = torch.sum(true_counts * logps, dim=-1)
+        return (-log_fact_sum + log_prod_fact - log_prod_exp).mean()
+
+
+class log1pMSELoss(nn.Module):
+    def __init__(self):
+        """
+From @jmschrei/bpnet-lite
+A MSE loss on the log(x+1) of the inputs.
+
+    This loss will accept tensors of predicted counts and a vector of true
+    counts and return the MSE on the log of the labels. The squared error
+    is calculated for each position in the tensor and then averaged, regardless
+    of the shape.
+
+    Note: The predicted counts are in log space but the true counts are in the
+    original count space.
+
+    Parameters
+    ----------
+    log_predicted_counts: torch.tensor, shape=(n, ...)
+            A tensor of log predicted counts where the first axis is the number of
+            examples. Important: these values are already in log space.
+
+    true_counts: torch.tensor, shape=(n, ...)
+            A tensor of the true counts where the first axis is the number of
+            examples.
+
+    Returns
+    -------
+    loss: torch.tensor, shape=(n, 1)
+            The MSE loss on the log of the two inputs, averaged over all examples
+            and all other dimensions.
+    """
+        super().__init__()
+
+    def forward(self, log_predicted_counts, true_counts):
+        log_true = torch.log(true_counts + 1)
+        return torch.mean(torch.square(log_true - log_predicted_counts))
 
 
 @dataclass
@@ -407,7 +475,7 @@ class GETRegionFinetune(BaseGETModel):
         return exp
 
     def before_loss(self, output, batch):
-        
+
         pred = {'exp': output}
         obs = {'exp': batch['exp_label']}
         return pred, obs
@@ -525,7 +593,7 @@ class GETChrombpNet(GETChrombpNetBias):
                 sample_peak_sequence, chunk_size, n_peaks, max_n_peaks, motif_mean_std)
             bias_atpm, bias_aprofile = bias_output['atpm'], bias_output['aprofile']
             atpm = torch.logsumexp(torch.stack(
-                [atpm, bias_atpm], dim=0), dim=0)
+                [atpm, torch.log(bias_atpm+1)], dim=0), dim=0)
             # diff_length = aprofile.shape[1] - bias_aprofile.shape[1]
             # crop_length = diff_length // 2
             # bias_aprofile = F.pad(
