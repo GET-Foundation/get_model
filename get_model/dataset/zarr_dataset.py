Fix off by one issue

get_model/dataset/zarr_dataset.py
@@ -1470,7 +1470,7 @@ class InferenceDataset(PretrainDataset):
 
     def _get_gene_info_from_window_idx(self, window_idx):
         # TODO: genome_chunk_length is affected by leave out chromosome by tss_chunk_idx is not
-        chunk_idx = window_idx % self.tss_chunk_idx.chunk_idx.max()
+        chunk_idx = window_idx % (self.tss_chunk_idx.chunk_idx.max()+1)
         gene_df = self.tss_chunk_idx.query(
             'chunk_idx==@chunk_idx or chunk_idx==@chunk_idx+1')
         return gene_df
@@ -1982,12 +1982,15 @@ class InferenceReferenceRegionDataset(Dataset):
         celltype_id = sample['metadata']['celltype_id']
         strand = sample['metadata']['strand']
         gene_name = sample['metadata']['gene_name']
+        print(gene_name)
         peak_end = peak_start + target.shape[0]
         region_motif, peaks = self.data_dict[celltype_id]
-        region_motif = region_motif[peak_start:peak_end]
-        chromosome = peaks['Chromosome'].values[peak_start]
-        peaks_coord = peaks[['Start', 'End']].values[peak_start:peak_end]
-        atpm = peaks['aTPM'].values[peak_start:peak_end]
+        rrd_peak_start = peaks.query('index_input==@peak_start').index.values[0]
+        rrd_peak_end = peaks.query('index_input==@peak_end').index.values[0]
+        region_motif = region_motif[rrd_peak_start:rrd_peak_end]
+        chromosome = peaks['Chromosome'].values[rrd_peak_start]
+        peaks_coord = peaks[['Start', 'End']].values[rrd_peak_start:rrd_peak_end]
+        atpm = peaks['aTPM'].values[rrd_peak_start:rrd_peak_end]
         # append binary or quantitative atac signal
         if not self.quantitative_atac:
             region_motif = np.concatenate(

test/test_inference_reference_region_dataset.py
@@ -1,5 +1,4 @@
 # %%
-from http.client import OK
 import seaborn as sns
 import torch.utils
 from get_model.run_ref_region import *
@@ -20,22 +19,22 @@ random.seed(0)
 gencode_config = {
     "assembly": "hg38",
     "version": 40,
-    "gtf_dir": "/home/xf2217/Projects/caesar/data/"
+    "gtf_dir": "/pmglocal/alb2281/get/get_data"
 }
 # Configuration for the dataset
 dataset_config = {
-    "zarr_dirs": ["/home/xf2217/Projects/get_data/joung_tfatlas_dense.zarr"],
-    "genome_seq_zarr": {'hg38': "/home/xf2217/Projects/get_data/hg38.zarr"},
-    "genome_motif_zarr": "/home/xf2217/Projects/get_data/hg38_motif_result.zarr",
+    "zarr_dirs": ["/pmglocal/alb2281/get/get_data/htan_gbm_dense.zarr"],
+    "genome_seq_zarr": {'hg38': "/pmglocal/alb2281/get/get_data/hg38.zarr"},
+    "genome_motif_zarr": "/pmglocal/alb2281/get/get_data/hg38_motif_result.zarr",
     "insulation_paths": [
-        "/home/xf2217/Projects/get_model/data/hg38_4DN_average_insulation.ctcf.adjecent.feather",
-        "/home/xf2217/Projects/get_model/data/hg38_4DN_average_insulation.ctcf.longrange.feather"
+        "/pmglocal/alb2281/repos/get_model/data/hg38_4DN_average_insulation.ctcf.adjecent.feather",
+        "/pmglocal/alb2281/repos/get_model/data/hg38_4DN_average_insulation.ctcf.longrange.feather"
     ],
-    "peak_name": "fetal_tfatlas_peaks_tissue_open_exp",
+    "peak_name": "fetal_gbm_peaks_open_exp",
     "leave_out_chromosomes": "",
     "additional_peak_columns": ["Expression_positive", "Expression_negative", "aTPM", "TSS"],
     "n_peaks_upper_bound": 900,
-    "keep_celltypes": "0.joung_tfatlas.L10M",
+    "keep_celltypes": "Tumor.htan_gbm.C3N-01814_CPT0167860015_snATAC_GBM_Tumor.16384",
     "center_expand_target": 0,
     "random_shift_peak": 0,
     "peak_count_filter": 3,
@@ -43,12 +42,12 @@ dataset_config = {
     "padding": 0,
 }
 # %%
-hg38 = DenseZarrIO('/home/xf2217/Projects/get_data/hg38.zarr')
+hg38 = DenseZarrIO('/pmglocal/alb2281/get/get_data/hg38.zarr')
 gencode = Gencode(**gencode_config)
 # %%
-gene_list = np.loadtxt(
-    '/home/xf2217/Projects/get_revision/TFAtlas_fetal_compare/diff_genes.txt', dtype=str)[0:10]
-
+# gene_list = np.loadtxt(
+#     '/home/xf2217/Projects/get_revision/TFAtlas_fetal_compare/diff_genes.txt', dtype=str)[0:10]
+gene_list = ['AARSD1', 'MYC', 'RET']
 dataset = InferenceDataset(
     assembly='hg38', gencode_obj={'hg38': gencode}, **dataset_config, gene_list=gene_list)
 # %%
@@ -59,18 +58,18 @@ rrm = ReferenceRegionMotif(cfg)
 rrd = InferenceReferenceRegionDataset(
     rrm, dataset, quantitative_atac=True, sampling_step=450)
 # %%
-rrd.__len__()
+rrd[0]
 
 # %%
 sum(rrd.zarr_dataset[0]['additional_peak_features']
     [:, 0:2] == rrd[0]['exp_label'])
 # %%
-rrd.data_dict['0.joung_tfatlas.L10M'][1].loc[rrd.zarr_dataset[0]
-                                             ['metadata']['original_peak_start']]
+rrd.data_dict['Tumor.htan_gbm.C3N-01814_CPT0167860015_snATAC_GBM_Tumor.16384'][1].loc[rrd.zarr_dataset[0]
+                                             ['metadata']['original_peak_start']+450]
 
-datapool_peak = rrd.zarr_dataset.datapool.peaks_dict['0.joung_tfatlas.L10M']
+datapool_peak = rrd.zarr_dataset.datapool.peaks_dict['Tumor.htan_gbm.C3N-01814_CPT0167860015_snATAC_GBM_Tumor.16384']
 
-rrd_peak = rrd.data_dict['0.joung_tfatlas.L10M'][1]
+rrd_peak = rrd.data_dict['Tumor.htan_gbm.C3N-01814_CPT0167860015_snATAC_GBM_Tumor.16384'][1]
 
 tss_coord = rrd.zarr_dataset[0]['metadata']['tss_coord']
 
@@ -81,4 +80,10 @@ tss_peak = rrd.zarr_dataset[0]['metadata']['tss_peak']
 tss_peak_df = datapool_peak.iloc[peak_start+tss_peak]
 # %%
 print(tss_coord > tss_peak_df.Start and tss_coord < tss_peak_df.End)
+# %%
+rrd_peak.query('index_input==@peak_start+@tss_peak')
+# %%
+tss_peak_df.Start==rrd_peak.query('index_input==@peak_start+@tss_peak').Start
+# %%
+rrd[1]
 # %%