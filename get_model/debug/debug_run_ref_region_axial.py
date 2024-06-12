4968c9f9c2f6ebe015599d39049c8802b2f0477f
Xi Fu <fuxialexander@gmail.com>
2024-06-12 13:54:02 -0400

    GETRegionFinetuneExpHiCAxial

---
+++ get_model/debug/debug_run_ref_region_axial.py
@@ -0,0 +1,19 @@
+import hydra
+import sys
+import gc
+import torch
+from get_model.run_ref_region import run
+
+from get_model.config.config import *
+
+
+@hydra.main(config_path="../config", config_name="eval_k562_fetal_ref_region_k562_hic_lora_axial", version_base="1.3")
+def main(cfg: Config):
+    run(cfg)
+
+
+if __name__ == "__main__":
+    main()
+    gc.collect()
+    torch.cuda.empty_cache()
+    sys.exit()
