b21c23e5b961d388e0d7fac9f488421aae9e7ffa
Alejandro Buendia <alb2281@columbia.edu>
2024-06-14 16:27:55 +0000

    Update aws config

--- scripts/run_gbm_dea_zeroshot.sh
+++ scripts/run_gbm_dea_zeroshot.sh
@@ -2,25 +2,25 @@
 set -e
 
 # run zero-shot gene DEA
-project_name="get-gbm-zeroshot-gene-dea"
-checkpoint="/home/ubuntu/alb2281/get/get_ckpts/astrocyte-natac-best.pth"
+project_name="get-gbm-dea-lora_watac"
+checkpoint="/home/ubuntu/alb2281/get/get_ckpts/watac_checkpoint_best.pth"
 
-sample_list_file="/home/ubuntu/alb2281/get/get_data/gbm_oom_samples.txt"
+sample_list_file="/home/ubuntu/alb2281/get/get_data/gbm_samples.txt"
 # read the sample list file into an array
 mapfile -t samples < "$sample_list_file"
 
 
 for sample in "${samples[@]}"; do
-    run_name="gbm_zeroshot_gene_dea_${sample}"
+    run_name="gbm_zeroshot_gene_dea_${sample}_watac"
     echo "Starting [$run_name]..."
-    CUDA_VISIBLE_DEVICES=1 python get_model/debug/debug_run_ref_region_gbm.py \
-        +machine=manitou_alb2281 \
+    CUDA_VISIBLE_DEVICES=0 python get_model/debug/debug_run_ref_region_gbm.py \
+        +machine=aws_alb2281 \
         stage=predict \
         dataset.peak_count_filter=10 \
         dataset.reference_region_motif.motif_scaler=1.3 \
         machine.num_devices=1 \
-        machine.batch_size=96 \
-        machine.num_workers=12 \
+        machine.batch_size=32 \
+        machine.num_workers=8 \
         wandb.project_name=$project_name \
         wandb.run_name=$run_name \
         finetune.checkpoint=$checkpoint \
