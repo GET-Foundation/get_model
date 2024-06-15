28bccab0b2a1715674aa0f86822ca8b851849223
Alejandro Buendia <alb2281@columbia.edu>
2024-06-15 02:00:36 -0400

    Update GBM dea launch scripts

--- scripts/run_gbm_dea_oneshot.sh
+++ scripts/run_gbm_dea_oneshot.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-# run one-shot gene DEA
-project_name="get-gbm-oneshot-gene-dea"
-checkpoint="/home/ubuntu/alb2281/get/get_ckpts/get_gbm_finetuned_C3L-03405_CPT0224600013.ckpt"
+project_name="get-gbm-dea-lora_watac"
+checkpoint="/pmglocal/alb2281/get/get_ckpts/gbm_Tumor_C3L-03405_lora.ckpt"
+output_dir="/pmglocal/alb2281/get/output/watac-oneshot-gbm"
 
-sample_list_file="/home/ubuntu/alb2281/get/get_data/gbm_oom_samples.txt"
+sample_list_file="/pmglocal/alb2281/get/get_data/gbm_samples.txt"
+# read the sample list file into an array
 mapfile -t samples < "$sample_list_file"
 
 
 for sample in "${samples[@]}"; do
-    run_name="gbm_oneshot_gene_dea_${sample}"
+    run_name="gbm_oneshot_gene_dea_${sample}_watac"
     echo "Starting [$run_name]..."
     CUDA_VISIBLE_DEVICES=0 python get_model/debug/debug_run_ref_region_gbm.py \
         +machine=manitou_alb2281 \
@@ -18,8 +19,8 @@ for sample in "${samples[@]}"; do
         dataset.peak_count_filter=10 \
         dataset.reference_region_motif.motif_scaler=1.3 \
         machine.num_devices=1 \
-        machine.batch_size=96 \
-        machine.num_workers=12 \
+        machine.batch_size=64 \
+        machine.num_workers=16 \
         wandb.project_name=$project_name \
         wandb.run_name=$run_name \
         finetune.checkpoint=$checkpoint \
--- scripts/run_gbm_dea_zeroshot.sh
+++ scripts/run_gbm_dea_zeroshot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# run zero-shot gene DEA
 project_name="get-gbm-dea-lora_watac"
-checkpoint="/home/ubuntu/alb2281/get/get_ckpts/watac_checkpoint_best.pth"
+checkpoint="/pmglocal/alb2281/get/get_ckpts/watac_checkpoint_best.pth"
+output_dir="/pmglocal/alb2281/get/output/watac-zeroshot-astrocyte"
 
-sample_list_file="/home/ubuntu/alb2281/get/get_data/gbm_samples.txt"
+sample_list_file="/pmglocal/alb2281/get/get_data/gbm_samples.txt"
 # read the sample list file into an array
 mapfile -t samples < "$sample_list_file"
 
@@ -14,13 +14,14 @@ for sample in "${samples[@]}"; do
     run_name="gbm_zeroshot_gene_dea_${sample}_watac"
     echo "Starting [$run_name]..."
     CUDA_VISIBLE_DEVICES=0 python get_model/debug/debug_run_ref_region_gbm.py \
-        +machine=aws_alb2281 \
+        +machine=manitou_alb2281 \
         stage=predict \
         dataset.peak_count_filter=10 \
         dataset.reference_region_motif.motif_scaler=1.3 \
         machine.num_devices=1 \
-        machine.batch_size=32 \
-        machine.num_workers=8 \
+        machine.batch_size=64 \
+        machine.num_workers=16 \
+        machine.output_dir=$output_dir \
         wandb.project_name=$project_name \
         wandb.run_name=$run_name \
         finetune.checkpoint=$checkpoint \
