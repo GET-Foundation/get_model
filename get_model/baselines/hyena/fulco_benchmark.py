5a22133c7fbb1f660e4c7892a472c1bdfe3d8a6a
Alejandro Buendia <alb2281@columbia.edu>
2024-06-25 03:57:12 -0400

    Fulco benchmark

--- get_model/baselines/hyena/fulco_benchmark.py
+++ get_model/baselines/hyena/fulco_benchmark.py
@@ -2,133 +2,63 @@ import json
 import os
 import subprocess
 import torch
-
-from transformers import PreTrainedModel
 import re
-from standalone_hyenadna import HyenaDNAModel
-from standalone_hyenadna import CharacterTokenizer
-from transformers import AutoModelForCausalLM
-
-from atac_rna_data_processing.io.region import Genome
-import os
 import pandas as pd
 import torch 
 from tqdm import tqdm
 import numpy as np
 from Bio.Seq import reverse_complement, Seq
 
+from transformers import PreTrainedModel
+from standalone_hyenadna import HyenaDNAModel
+from standalone_hyenadna import CharacterTokenizer
+from transformers import AutoModelForCausalLM
+import pathlib
+
+from atac_rna_data_processing.io.region import Genome
+
 
 SEQUENCE_LENGTH = 1_000_000
 WINDOW_SIZE = 2000
+SAVE_EVERY = 3
 
 
-# helper 1
-def inject_substring(orig_str):
-    """Hack to handle matching keys between models trained with and without
-    gradient checkpointing."""
+def single_inference(row, model):
+    with torch.inference_mode():
+        if row["contains_enhancer"] == False:
+            row["region_logits"] = []
+            row["knockout_logits"] = []
+            output_row = row[["orig_idx", "chrom", "chromStart", "chromEnd", "region_logits", "knockout_logits"]]
+        else:
+            row["attention_start"] = row["startTSS"] - WINDOW_SIZE // 2
+            row["attention_end"] = row["startTSS"] + WINDOW_SIZE // 2
 
-    # modify for mixer keys
-    pattern = r"\.mixer"
-    injection = ".mixer.layer"
+            start_idx = int(row["attention_start"]) - row["region_start"]
+            end_idx = int(row["attention_end"]) - row["region_start"]
 
-    modified_string = re.sub(pattern, injection, orig_str)
+            input_ids = np.array(tokenizer(row["region_dna_sequence"])["input_ids"])
+            region_token_seq = torch.LongTensor(input_ids).unsqueeze(0)  # unsqueeze for batch dim
+            region_tok_seq = region_token_seq.to(device)
+            region_logits = model(region_tok_seq).logits.cpu().detach().numpy()
+            region_logits = region_logits[0, np.arange(start_idx, end_idx), input_ids[start_idx:end_idx]].tolist()  # grab logits corresponding to sequence
+            row["region_logits"] = region_logits
 
-    # modify for mlp keys
-    pattern = r"\.mlp"
-    injection = ".mlp.layer"
-
-    modified_string = re.sub(pattern, injection, modified_string)
-
-    return modified_string
-
-# helper 2
-def load_weights(scratch_dict, pretrained_dict, checkpointing=False):
-    """Loads pretrained (backbone only) weights into the scratch state dict."""
-
-    # loop thru state dict of scratch
-    # find the corresponding weights in the loaded model, and set it
-
-    # need to do some state dict "surgery"
-    for key, value in scratch_dict.items():
-        if 'backbone' in key:
-            # the state dicts differ by one prefix, '.model', so we add that
-            key_loaded = 'model.' + key
-            # breakpoint()
-            # need to add an extra ".layer" in key
-            if checkpointing:
-                key_loaded = inject_substring(key_loaded)
-            try:
-                scratch_dict[key] = pretrained_dict[key_loaded]
-            except:
-                raise Exception('key mismatch in the state dicts!')
-
-    # scratch_dict has been updated
-    return scratch_dict
-
-
-def inference_batch(data_df, running_file):
-    model = AutoModelForCausalLM.from_pretrained("LongSafari/hyenadna-large-1m-seqlen-hf", trust_remote_code=True)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    print("Using device:", device)
-
-    pretrained_model_name = 'hyenadna-large-1m-seqlen'
-    max_lengths = {
-        'hyenadna-tiny-1k-seqlen': 1024,
-        'hyenadna-small-32k-seqlen': 32768,
-        'hyenadna-medium-160k-seqlen': 160000,
-        'hyenadna-medium-450k-seqlen': 450000,  # T4 up to here
-        'hyenadna-large-1m-seqlen': 1_000_000,  # only A100 (paid tier)
+            knockout_input_ids = np.array(tokenizer(row["region_dna_sequence_knockout"])["input_ids"])
+            knockout_token_seq = torch.LongTensor(knockout_input_ids).unsqueeze(0)  # unsqueeze for batch dim
+            knockout_tok_seq = knockout_token_seq.to(device)
+            knockout_logits = model(knockout_tok_seq).logits.cpu().detach().numpy()
+            knockout_logits = knockout_logits[0, np.arange(start_idx, end_idx), knockout_input_ids[start_idx:end_idx]].tolist()  # grab logits corresponding to sequence
+            row["knockout_logits"] = knockout_logits
+      
+    output_dict = {
+        "orig_idx": row["orig_idx"],
+        "chrom": row["chrom"],
+        "chromStart": row["chromStart"],
+        "chromEnd": row["chromEnd"],
+        "region_logits": row["region_logits"],
+        "knockout_logits": row["knockout_logits"]
     }
-    max_length = max_lengths[pretrained_model_name]  # auto selects
-
-    # create tokenizer
-    tokenizer = CharacterTokenizer(
-        characters=['A', 'C', 'G', 'T', 'N'],  # add DNA characters, N is uncertain
-        model_max_length=max_length + 2,  # to account for special tokens, like EOS
-        add_special_tokens=False,  # we handle special tokens elsewhere
-        padding_side='left', # since HyenaDNA is causal, we pad on the left
-    )
-
-    # prep model and forward
-    model.to(device)
-    model.eval()
-
-    with open(running_file, "w") as output_f:
-        with torch.inference_mode():
-            #### Single embedding example ####
-            for idx, row in tqdm(data_df.iterrows(), total=len(data_df)):
-                if not row["contains_enhancer"]:
-                    row["region_score"] = np.nan
-                    row["knockout_score"] = np.nan
-                    output_row = row[["orig_idx", "chrom", "chromStart", "chromEnd", "region_score", "knockout_score"]]
-                    output_f.write("\t".join(map(str, output_row.values)) + "\n")
-                    output_f.flush()
-                else:
-                    row["attention_start"] = row["startTSS"] - WINDOW_SIZE // 2
-                    row["attention_end"] = row["startTSS"] + WINDOW_SIZE // 2
-
-                    start_idx = row["attention_start"] - row["region_start"]
-                    end_idx = row["attention_end"] - row["region_start"]
-
-                    input_ids = np.array(tokenizer(row["region_dna_sequence"])["input_ids"])
-                    region_token_seq = torch.LongTensor(input_ids).unsqueeze(0)  # unsqueeze for batch dim
-                    region_tok_seq = region_token_seq.to(device)
-                    region_logits = model(region_tok_seq).logits.cpu().detach().numpy()
-                    region_logits = region_logits[0, np.arange(start_idx, end_idx), input_ids]  # grab logits corresponding to sequence
-                    row["region_score"] = np.sum(region_logits)
-
-                    knockout_input_ids = np.array(tokenizer(row["region_dna_sequence_knockout"])["input_ids"])
-                    knockout_token_seq = torch.LongTensor(knockout_input_ids).unsqueeze(0)  # unsqueeze for batch dim
-                    knockout_tok_seq = knockout_token_seq.to(device)
-                    knockout_logits = model(knockout_tok_seq).logits.cpu().detach().numpy()
-                    knockout_logits = knockout_logits[0, np.arange(start_idx, end_idx), knockout_input_ids]  # grab logits corresponding to sequence
-                    row["knockout_score"] = np.sum(knockout_logits)
-
-                    output_row = row[["orig_idx", "chrom", "chromStart", "chromEnd", "region_score", "knockout_score"]]
-                    output_f.write("\t".join(map(str, output_row.values)) + "\n")
-                    output_f.flush()
-
-    return
+    return output_dict
 
 
 def replace_enhancer_str(region_str, enhancer_str):
@@ -151,8 +81,7 @@ if __name__=="__main__":
     fulco_df["region_end"] = fulco_df["region_end"].apply(lambda x: int(x))
 
     fulco_df["enhancer_seq"] = fulco_df.apply(lambda x: genome.get_sequence(x["chrom"], x["chromStart"], x["chromEnd"]).seq, axis=1)
-    fulco_df["region_dna_sequence"] = fulco_df.apply(lambda x: genome.get_sequence(x["chrom"], x["region_start"], x["region_end"]), axis=1)
-    # check if enhancer string in region string
+    fulco_df["region_dna_sequence"] = fulco_df.apply(lambda x: genome.get_sequence(x["chrom"], x["region_start"], x["region_end"]).seq, axis=1)
     fulco_df["contains_enhancer"] = fulco_df.apply(lambda x: x["enhancer_seq"] in x["region_dna_sequence"], axis=1)
 
     fulco_df["region_dna_sequence"] = fulco_df["region_dna_sequence"].apply(lambda x: Seq(x))
@@ -160,7 +89,40 @@ if __name__=="__main__":
     # if enhancer string is to the right of the TSS, take the reverse complement of the region and enhancer string (causal Hyena)   
     fulco_df["region_dna_sequence"] = fulco_df.apply(lambda x: str(x["region_dna_sequence"]) if x["chromStart"] < x["startTSS"] else str(x["region_dna_sequence"].reverse_complement()), axis=1)
     fulco_df["enhancer_seq"] = fulco_df.apply(lambda x: str(x["enhancer_seq"]) if x["chromStart"] < x["startTSS"] else str(x["enhancer_seq"].reverse_complement()), axis=1)
-
     fulco_df["region_dna_sequence_knockout"] = fulco_df.apply(lambda x: replace_enhancer_str(x["region_dna_sequence"], x["enhancer_seq"]), axis=1)
-    running_file = "/pmglocal/alb2281/get/results/hyena-1m/hyena_dna_1m_context_fulco_scores_progress.tsv"
-    region_scores, enhancer_scores = inference_batch(fulco_df, running_file)
+
+    model = AutoModelForCausalLM.from_pretrained("LongSafari/hyenadna-large-1m-seqlen-hf", trust_remote_code=True)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print("Using device:", device)
+
+    pretrained_model_name = 'hyenadna-large-1m-seqlen'
+    max_lengths = {
+        'hyenadna-tiny-1k-seqlen': 1024,
+        'hyenadna-small-32k-seqlen': 32768,
+        'hyenadna-medium-160k-seqlen': 160000,
+        'hyenadna-medium-450k-seqlen': 450000,  # T4 up to here
+        'hyenadna-large-1m-seqlen': 1_000_000,  # only A100 (paid tier)
+    }
+    max_length = max_lengths[pretrained_model_name]  # auto selects
+
+    tokenizer = CharacterTokenizer(
+        characters=['A', 'C', 'G', 'T', 'N'],  # add DNA characters, N is uncertain
+        model_max_length=max_length + 2,  # to account for special tokens, like EOS
+        add_special_tokens=False,  # we handle special tokens elsewhere
+        padding_side='left', # since HyenaDNA is causal, we pad on the left
+    )
+
+    model.to(device)
+    model.eval()
+
+    output_dir = "/pmglocal/alb2281/get/results/hyena-fulco"
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    result_col = []
+    # iterate over rows of dataframe
+    for idx, row in tqdm(fulco_df.iterrows(), total=len(fulco_df)):
+        if idx % SAVE_EVERY == 0:
+            # save results to json
+            with open(f"hyena_fulco_benchmark_chunk_{idx}.json", "w") as f:
+                json.dump(result_col, f)
+        result_col.append(single_inference(row, model))
