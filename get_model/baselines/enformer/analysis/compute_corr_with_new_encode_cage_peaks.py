475e1b03d4a05d61b009ec5e1fc8b1135c79bbed
Alejandro Buendia <alb2281@columbia.edu>
2024-06-27 13:48:52 -0400

    Updated eval for k562 encode

--- get_model/baselines/enformer/analysis/compute_corr_with_new_encode_cage_peaks.py
+++ get_model/baselines/enformer/analysis/compute_corr_with_new_encode_cage_peaks.py
@@ -4,7 +4,10 @@ import json
 import numpy as np
 from tqdm import tqdm
 # %%
-preds_dir = "/pmglocal/alb2281/repos/get_model/get_model/baselines/enformer/preds/enformer_k562_cage"
+cage_peaks = "/burg/pmg/users/alb2281/get/get_data/cage_peaks.csv"
+
+# %%
+preds_dir = "/burg/pmg/users/alb2281/enformer/results/enformer_k562_new_encode_chr10,11"
 # %%
 pred_files = os.listdir(preds_dir)
 # %%
@@ -14,8 +17,11 @@ pred_files = sorted(pred_files, key=lambda x: int(x.split("_")[-1].split(".")[0]
 pred_files.append("enformer_cage_example_final.json")
 # %%
 
-leaveout_cage_peaks = "/pmglocal/alb2281/repos/get_model/get_model/baselines/enformer/data/cage_peaks_leaveout_k562.csv"
-leaveout_df = pd.read_csv(leaveout_cage_peaks)
+cage_peaks_df = pd.read_csv(cage_peaks)
+
+
+# %%
+cage_peaks_df = cage_peaks_df.query('Chromosome == "chr10" or Chromosome == "chr11"')
 # %%
 
 all_preds = []
@@ -39,10 +45,63 @@ combined_preds.keys()
 len(combined_preds['4828'].keys())
 len(combined_preds['5111'].keys())
 # %%
+
+leaveout_df = cage_peaks_df.copy()
+# %%
 leaveout_df["Expression_sum"] = leaveout_df["Expression_positive"] + leaveout_df["Expression_negative"]
 # %%
 leaveout_df["Expression_sum"].describe()
 #%%
+# leaveout_df = leaveout_df.query('TSS>0')
+# %%
+mean_preds_4828 = []
+for orig_idx in leaveout_df.index.values:
+    mean_preds_4828.append(np.mean(combined_preds['4828'][str(orig_idx)]))
+
+# %%
+mean_preds_5111 = []
+for orig_idx in leaveout_df.index.values:
+    mean_preds_5111.append(np.mean(combined_preds['5111'][str(orig_idx)]))
+# %%
+mean_preds_4828
+# %%
+leaveout_df["enformer_track_4828"] = mean_preds_4828
+leaveout_df["enformer_track_5111"] = mean_preds_5111
+# %%
+leaveout_df["enformer_mean"] = (leaveout_df["enformer_track_4828"] + leaveout_df["enformer_track_5111"]) / 2
+# %%
+
+# pearson between enformer_mean and expression sum
+leaveout_df[["enformer_mean", "Expression_sum"]].corr()
+# %%
+
+eval_df = leaveout_df[['Expression_positive', 'Expression_negative', 'enformer_mean', 'aTPM', 'Expression_sum', 'enformer_track_4828', 'enformer_track_5111', 'enformer_mean']]
+# %%
+eval_df.corr(method='pearson')
+# %%
+eval_df.corr(method='spearman')
+# %%
+
+sum_preds_4828 = []
+for orig_idx in leaveout_df.index.values:
+    sum_preds_4828.append(np.sum(combined_preds['4828'][str(orig_idx)]))
+
+# %%
+eval_df["enformer_track_4828_sum"] = sum_preds_4828
+# %%
+
+sum_preds_5111 = []
+for orig_idx in leaveout_df.index.values:
+    sum_preds_5111.append(np.sum(combined_preds['5111'][str(orig_idx)]))
+# %%
+
+eval_df["enformer_track_5111_sum"] = sum_preds_5111
+#%%
+
+eval_df.corr(method='pearson')
+# %%
+
+# TSS only
 leaveout_df = leaveout_df.query('TSS>0')
 # %%
 mean_preds_4828 = []
@@ -82,40 +141,12 @@ eval_df["enformer_track_4828_sum"] = sum_preds_4828
 # %%
 
 sum_preds_5111 = []
-for orig_idx in range(len(leaveout_df)):
+for orig_idx in leaveout_df.index.values:
     sum_preds_5111.append(np.sum(combined_preds['5111'][str(orig_idx)]))
 # %%
 
 eval_df["enformer_track_5111_sum"] = sum_preds_5111
 #%%
-max_preds_4828 = []
-for orig_idx in leaveout_df.index.values:
-    if len(combined_preds['4828'][str(orig_idx)]) == 0:
-        max_preds_4828.append(np.nan)
-    else:
-        max_preds_4828.append(np.array(combined_preds['4828'][str(orig_idx)]).max())
 
-# %%
-eval_df["enformer_track_4828_max"] = max_preds_4828
-# %%
-
-max_preds_5111 = []
-for orig_idx in leaveout_df.index.values:
-    if len(combined_preds['5111'][str(orig_idx)]) == 0:
-        max_preds_5111.append(np.nan)
-    else:
-        max_preds_5111.append(np.max(combined_preds['5111'][str(orig_idx)]))
-# %%
-
-eval_df["enformer_track_5111_max"] = max_preds_5111
-# %%
-
-eval_df.dropna().corr()
-# %%
-eval_df["enformer_max_mean"] = (eval_df["enformer_track_4828_max"] + eval_df["enformer_track_5111_max"]) / 2
-# %%
-
-eval_df.corr(method='spearman')
-# %%
-eval_df.dropna().plot(x="enformer_max_mean", y="Expression_sum", kind="scatter")
+eval_df.corr(method='pearson')
 # %%
