18a2d67cbbb30f97c2b4b9b3f8204cfb06b5c954
Alejandro Buendia <alb2281@columbia.edu>
2024-07-01 17:45:05 -0400

    Clean up Hyena benchmarks

--- get_model/baselines/hyena/launch_finetune_hyena.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python /pmglocal/alb2281/repos/get_model/get_model/baselines/hyena/train_hyena.py \
-    --data_dir /pmglocal/alb2281/repos/get_model/get_model/baselines/hyena/data \
-    --batch_size 400 \
-    --num_epochs 100 \
-    --wandb_project_name get-cre-pred-hyena \
-    --wandb_run_name cre_pred_hyena_10_balanced_cls \
-    --split_data \
-    --balanced_split
